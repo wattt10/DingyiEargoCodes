f787e9c6824aa4dc7fceb5889314779ef692523a
wattt10 <46799636+wattt10@users.noreply.github.com>
2019-11-15 07:32:37 -0800

    Update Exercise3.java

--- Exercise3.java
+++ Exercise3.java
@@ -4,10 +4,11 @@ public class Exercise3 {
 		if (stockPrices.length == 0) {
 		      return 0;
 		    }
+		 //Maintain a current minimun and a current possible max gain
 		    int min= stockPrices[0];
 		    int gain = 0;
 		    for(int i = 1; i < stockPrices.length; i++) {
-		     
+		     // update them at each day
 		        gain = Math.max(stockPrices[i] - min, gain);
 		        if (stockPrices[i] < min){
 		          min = stockPrices[i];
@@ -20,6 +21,7 @@ public class Exercise3 {
 public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	Exercise3 s = new Exercise3();
+	//Test Case
 	System.out.println(s.maxProfit(new int[] {3,8,8,55,38,1,7,42,54,53}));
 }
 }
